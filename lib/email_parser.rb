7def9823c635c9dbbca3f75c944dfd1bb4d45a77
Evans Ochieng <evansochieng121@gmail.com>
2022-10-21 09:48:43 +0300

    practice parsing emails using regex

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,30 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+require "pry"
+
+# EmailAddressParser
+class EmailAddressParser
+    #initialize unformatted emails as an array
+    def initialize(emails)
+        @emails = emails
+    end
+
+    #parse the method
+    def parse
+        # Criteria for split: ", " for csv | " " for white space
+        formatedEmails = @emails.split(/, | /)
+        #binding.pry
+
+        #return only unique emails
+        formatedEmails.uniq
+    end
+end
+
+# Try it out
+email_addresses = "john@doe.com person@somewhere.org"
+parser = EmailAddressParser.new(email_addresses)
+
+# parser.parse
+EmailAddressParser.new("avi@test.com, avi@test.com").parse
